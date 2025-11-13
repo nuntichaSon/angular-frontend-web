66d4a71e30eb149815222844f0e2a2595a0262c2
nuntichaSon <nunticha.son@gmail.com>
2025-11-13 16:11:23 +0700

    fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app-routing.module.routes';
 
-
-
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
